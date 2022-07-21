27e8902b333e35c152451f9bd32606cfc135a9be
Marion Perez <marion.oceane.perez@gmail.com>
2022-07-21 16:57:18 +0200

    ajout d'un dto encore vide

---
+++ src/main/java/com/poe/crmSpringboot/api/dto/ClientDto.java
@@ -0,0 +1,8 @@
+package com.poe.crmSpringboot.api.dto;
+
+// Dto = data transfert objet
+// Modèle indépendant qui définit la façon dont les données seront envoyées sur le réseau
+// Permet de ne pas exposer les entités de la base de données au client qui ne recevra plus de données
+// Correspondant directement à la bdd mais une forme modifiée
+public class ClientDto {
+}
