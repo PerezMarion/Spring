9f79ad29c6de5c6f6dfe78c397a8feed36788ac5
Marion Perez <marion.oceane.perez@gmail.com>
2022-07-21 10:05:26 +0200

    modification de la classe Service en utilisant l'interface ClientRepository

--- src/main/java/com/poe/crmSpringboot/business/service/ServicesClientOrder.java
+++ src/main/java/com/poe/crmSpringboot/business/service/ServicesClientOrder.java
@@ -1,8 +1,9 @@
 package com.poe.crmSpringboot.business.service;
 
 import com.poe.crmSpringboot.business.Client;
+import com.poe.crmSpringboot.dao.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,46 +12,83 @@ import java.util.Optional;
 @Service
 public class ServicesClientOrder {
 
-    ArrayList<Client> clients = new ArrayList<>();
-    long index = 0;
+// -------------------------- Avec utilisation de l'interface ClientRepository ----------------------------- //
+
+    @Autowired
+    ClientRepository clientRepository;
 
     public List<Client> getAllClients() {
-        return clients;
+        return clientRepository.findAll();
     }
 
     public Optional<Client> findClientById(Long id) {
-        for(Client client : clients) {
-            if(client.getId().equals(id)) {
-                return Optional.of(client);
-            }
-        }
-        return Optional.empty();
+        return clientRepository.findById(id);
     }
 
     public void addClient(Client client) {
-        index++;
-        client.setId(index);
-        clients.add(client);
+        clientRepository.save(client);
     }
 
     public boolean deleteClient(Long id) {
-        for(Client client : clients) {
-            if(client.getId().equals(id)) {
-                clients.remove(client);
-                return true;
-            }
+        if(clientRepository.existsById(id)) {
+            clientRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
         }
-        return false;
     }
 
-    public boolean updateClient(Client newDataClient) {
-        for(Client oldDataClient : clients) {
-            if(oldDataClient.getId().equals(newDataClient.getId())) {
-                clients.remove(oldDataClient);
-                clients.add(newDataClient);
-                return true;
-            }
+    public boolean updateClient(Client client) {
+        if(clientRepository.existsById(client.getId())) {
+            clientRepository.save(client);
+            return true;
+        } else {
+            return false;
         }
-        return false;
     }
+
+// ------------------------- Avant utilisation de l'interface ClientRepository ----------------------------- //
+
+    //ArrayList<Client> clients = new ArrayList<>();
+    //long index = 0;
+
+    //public List<Client> getAllClients() {
+        //return clients;
+    //}
+
+    //public Optional<Client> findClientById(Long id) {
+        //for(Client client : clients) {
+            //if(client.getId().equals(id)) {
+                //return Optional.of(client);
+            //}
+        //}
+        //return Optional.empty();
+    //}
+
+    //public void addClient(Client client) {
+        //index++;
+        //client.setId(index);
+        //clients.add(client);
+    //}
+
+    //public boolean deleteClient(Long id) {
+        //for(Client client : clients) {
+            //if(client.getId().equals(id)) {
+                //clients.remove(client);
+                //return true;
+            //}
+        //}
+        //return false;
+    //}
+
+    //public boolean updateClient(Client newDataClient) {
+        //for(Client oldDataClient : clients) {
+            //if(oldDataClient.getId().equals(newDataClient.getId())) {
+                //clients.remove(oldDataClient);
+                //clients.add(newDataClient);
+                //return true;
+            //}
+        //}
+        //return false;
+    //}
 }
--- src/main/java/com/poe/crmSpringboot/dao/ClientRepository.java
+++ src/main/java/com/poe/crmSpringboot/dao/ClientRepository.java
@@ -4,8 +4,12 @@ import com.poe.crmSpringboot.business.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-// Création d'une interface ClientRepository (sans annotation) qui étend JpaRepository
+// Création d'une interface ClientRepository qui étend JpaRepository
 // Cette interface sera directement "injectable" sans aucune implémentation (@Autowired)
 // Elle est sous la forme JpaRepository<ENTITE,TYPE CLE PRIMAIRE>
+// Elle nous permet de générer toutes sortes de méthodes vers la base de données sans que l'on ait à écrire
+// La moindre requête ou implémentation Dao
+// Méthodes disponibles (entre autres) : findAll(), findById(), save(), saveAll(), delete(), deleteAll(),
+// deleteById(), count(), existsById(),
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> { }
